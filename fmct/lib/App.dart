2dd138817ca44a9eb2fda63a632aae272a97985b
zbc <1511828529@qq.com>
2023-05-22 11:30:49 +0800

    shake method

--- fmct/lib/App.dart
+++ fmct/lib/App.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: file_names, use_build_context_synchronously
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'package:shake/shake.dart';
 
 import 'appConfig.dart';
 import './utils/main.dart';
+import './service/main.dart';
 
 import 'h5Channels/main.dart';
 import './pages/Ipconfig.dart';
@@ -23,6 +25,23 @@ class AppState extends State<App> {
   @override
   void initState() {
     super.initState();
+    if (Configure.debugging) {
+      // 监听摇一摇事件
+      ShakeDetector detector = ShakeDetector.autoStart(
+        onPhoneShake: () async {
+          String? result = await ModalConfirm.show(
+              context, "您想要重新配置ip地址吗？", "通过配置 ip 来决定您将要访问的app地址");
+          if (result == "true") {
+            ipConfig();
+          }
+          // Do stuff on phone shake
+        },
+        minimumShakeCount: 1,
+        shakeSlopTimeMS: 500,
+        shakeCountResetTime: 3000,
+        shakeThresholdGravity: 2.7,
+      );
+    }
     AppConfig.getH5url().then((res) {
       setState(() {
         _appUrl = res;
